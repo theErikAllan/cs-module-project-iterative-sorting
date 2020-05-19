1862684b53ee3c14fac0a7f292e576322f7935f3
Rick Ahlgren <soul@DESKTOP-MPL9BMJ.attlocal.net>
2020-05-19 15:02:16 -0400

    Wrote more comments and added solution for stretch problem using Sean's code from lecture

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,8 +2,8 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        # First, we set two variables, one to track the current index, and one to track the smallest index of the unsorted values
-        # This allows us to compare the value at the current index with the value at the smallest index
+        # First, we set two variables, one to track the current index as we move through the array, and one to track the index of the smallest value
+        # This allows us to compare the value at the current index with the smallest value 
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
@@ -12,11 +12,12 @@ def selection_sort(arr):
         
         # Here, we write a for loop to go through the rest of the array, starting from the index of the last element to be sorted
         for j in range(cur_index + 1, len(arr)):
-            # For each index, we compared the value at the j index with the value at the smallest index
+            # In each loop, we compare the value at the j index with the smallest value
             if arr[j] < arr[smallest_index]:
-                # If the value at j is smaller than the value at the smallest index, we set the smallest index to be j
+                # If the value at j is smaller than the value at the smallest index, we set the index of the smallest value to be j, as j represents the index of the smallest value up to this point
                 smallest_index = j
         
+        # Finally, we swap the value at the current index with the smallest value we found during the loop
         arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
 
@@ -29,15 +30,54 @@ def selection_sort(arr):
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-    
+    # First, we determine the length of the array for the for loop we will run 
+    arr_len = len(arr)
+
+    # Next, we set the for loop to run up to the last index, aka (arr_len - 1)
+    for i in range(arr_len - 1):
+        # Then we create a nested for loop that starts at the beginning of the array and goes through the remaining array elements: (arr_len - i -1) represents the length of the array minus the number of elements already sorted by the first for loop
+        for j in range(0, arr_len - i - 1):
+            # Next, for each element, we check to see if the value at the current index is greater than the value to the right of it
+            if arr[j] > arr[j + 1]:
+                # If the current value is greater than the one to the right, we swap the elements before resuming the for loop
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
+# Requires us to know the "max" value that we'll be sorting
+# The maximum was arg so we could specify the max value
+# The total range of data we'll be sorting sits between 0 and maximum
 def count_sort(arr, maximum=-1):
     # Your code here
+    if len(arr) == 0:
+        return arr
+    
+    # if maximum is not given, we'll take the max value from the input array
+    if maximum == -1:
+        maximum = max(arr)
 
+    # make a bunch of buckets
+    buckets = [0 for i in range(maximum + 1)]
+
+    for x in arr:
+        if x < 0:
+            return "Error, negative numbers not allowed"
+        buckets[x] += 1
+    
+    # We have the counts of every value in our input array
+    # Loop through our buckets, starting at the smallest index
+    j = 0
+    for i in range(len(buckets)):
+        while buckets[i] > 0:
+            arr[j] = i
+            j += 1
+            buckets[i] -= 1
 
     return arr
+
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+
+print(count_sort(arr1))
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,33 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
     # Your code here
+    # First, we set variables to the min and max indices
+    # 0 is the first index
+    # len(arr) - 1 is the final index
+    low = 0
+    high = len(arr) - 1
 
+    # Then we write a while loop that runs as long as the lowest index is less than or equal to the highest, representing the narrowing of the possible indices which contain the target value
+    while low <= high:
+        # We initially grab the middle index value as a starting point, and convert it to an integer in case there are an even number of elements in the array
+        middle = int((low + high) / 2)
 
+        # Then we check to see if the target value could be in the left side of the array by checking if it's less than the element in the middle
+        if target < arr[middle]:
+            # If the value of the target element is less than the value at the middle index, we update the high index by removing the "right half" of the array
+            high = middle - 1
+        elif target > arr[middle]:
+            # If the value of the target element is greater than the value at the middle index, we update the low index by removing the "left half" of the array
+            low = middle + 1
+        else:
+            # Finally, if the value of the target element is not less than or greater than the value at the middle index, it must be equal, so we return the value of the final middle index
+            return middle
     return -1  # not found
